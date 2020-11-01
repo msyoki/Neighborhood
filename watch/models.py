531e47b53f55692e9c76c005da91872face430c8
msyoki <msyokimutua@gmail.com>
2020-11-01 12:42:13 +0300

    agdd neighborhood  class

--- watch/models.py
+++ watch/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Neighborhood(models.Model):
+    name=models.CharField(max_length=40)
+    location=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic=models.ImageField(upload_to='profilepic/',default='default.jpeg')
@@ -8,7 +15,7 @@ class Profile(models.Model):
     last_name=models.CharField(max_length=30)
     bio= models.CharField(max_length=250)
     email=models.EmailField()
+    neighborhood=models.ForeignKey(Neighborhood, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.first_name
-    
