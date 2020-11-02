bd3021acd859c842c381885ae1bea29e2fb8f903
msyoki <msyokimutua@gmail.com>
2020-11-02 12:45:55 +0300

    add new business user form and render

--- watch/forms.py
+++ watch/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile,Alert
+from .models import Profile,Alert,Business
 
 
 class RegisterForm(UserCreationForm):
@@ -20,3 +20,9 @@ class AlertForm(forms.ModelForm):
     class Meta:
         model=Alert
         exclude=['post_on','user']
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['']
